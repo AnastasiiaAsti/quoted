f79c7807c14b924d2758e84a61a4ea56e2642a68
Asti <astistateofmind@gmail.com>
2023-06-21 15:03:02 -0700

    fixed env issue

--- src/utls/firebaseConfig.js
+++ src/utls/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDrDHI5AaS0cCw2BVSXsQktfyHPYk1gcz4',
-    authDomain: 'quoteverse-3990a.firebaseapp.com',
-    projectId: 'quoteverse-3990a',
-    storageBucket: 'quoteverse-3990a.appspot.com',
-    messagingSenderId: '171232950013',
-    appId: '1:171232950013:web:3ab6db27ebb5fc21b7f094',
-    measurementId: 'G-LBPWNGF0J4'
+    apiKey: process.env.NEXT_PUBLIC_API,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASURE_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
